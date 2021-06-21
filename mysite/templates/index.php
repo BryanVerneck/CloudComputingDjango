5e4b3eddc5ba6be2b013940e5489e9e7ce62f8ff
Bryan verneck <bryanverneck@gmail.com>
2021-06-21 19:46:33 -0300

    projeto Heroku

--- mysite/templates/index.php
+++ mysite/templates/index.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * @author Marco Nurmberg
- * @date 21/06/2021
- */
 
 $dataAgora = date(DATE_RFC822);
 $nomeUsuario = "Bryan Higino Verneck";
@@ -10,19 +6,4 @@ $nomeUsuario = "Bryan Higino Verneck";
 echo "Aluno: {$nomeUsuario} <br>";
 echo "Data e hora: {$dataAgora} <br>";
 
-// from django.shortcuts import render
-// from django.http import HttpResponse
-// import datetime
-//
-// def page5(request):
-//     data_atual = datetime.date.today()
-//
-//     html = f'''
-//     <html>
-//         <body>
-//             <h1>Nome: Bryan Higino Verneck</h1>
-//             <h1> Data: {data_atual} </h1>
-//         </body>
-//     </html>
-//     '''
-//     return HttpResponse(html)
+?>
